d040fc4f4e116d0429b2f2920fd008fc2e39ba52
Mudit Sahni <muditsahni@msn.com>
2024-07-07 12:50:41 +0530

    Update ApplicationTest.kt

--- {{ cookiecutter.artifact_id_slug }}/src/test/kotlin/{{ cookiecutter.group_domain_type }}/{{ cookiecutter.group_name }}/{{ cookiecutter.artifact_id_slug }}/ApplicationTest.kt
+++ {{ cookiecutter.artifact_id_slug }}/src/test/kotlin/{{ cookiecutter.group_domain_type }}/{{ cookiecutter.group_name }}/{{ cookiecutter.artifact_id_slug }}/ApplicationTest.kt
@@ -1,4 +1,4 @@
-package {{ cookiecutter.group_domain_name }}.{{ cookiecutter.group_name }}.{{ cookiecutter.artifact_id_slug }}
+package {{ cookiecutter.group_domain_type }}.{{ cookiecutter.group_name }}.{{ cookiecutter.artifact_id_slug }}
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
