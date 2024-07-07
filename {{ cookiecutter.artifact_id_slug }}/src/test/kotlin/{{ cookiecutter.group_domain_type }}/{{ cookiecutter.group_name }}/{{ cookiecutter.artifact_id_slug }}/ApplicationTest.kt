ba4dd9ceb3349c7f2196b320f46f1ac6f2e72f5a
Mudit Sahni <muditsahni@msn.com>
2024-07-07 12:46:28 +0530

    Create ApplicationTest.kt

---
+++ {{ cookiecutter.artifact_id_slug }}/src/test/kotlin/{{ cookiecutter.group_domain_type }}/{{ cookiecutter.group_name }}/{{ cookiecutter.artifact_id_slug }}/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package {{ cookiecutter.group_domain_name }}.{{ cookiecutter.group_name }}.{{ cookiecutter.artifact_id_slug }}
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
